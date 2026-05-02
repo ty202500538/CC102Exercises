646464558bba1042b1c1ba0212661641f6bed79f
ty202500538 <ty202500538@wmsu.edu.ph>
2026-05-03 00:35:34 +0800

    Update P1_Q7_2.cpp

--- P1_Q7_2.cpp
+++ P1_Q7_2.cpp
@@ -4,9 +4,9 @@
 
 using namespace std;
 
-bool isPrime(int num) {
-    if (num <= 1) return false;
-    for (int i = 2; i <= sqrt(num); i++) {
+bool isPrime(int num){
+    if(num<=1) return false;
+    for(int i = 2; i <= sqrt(num); i++) {
         if (num % i == 0) return false;
     }
     return true;
