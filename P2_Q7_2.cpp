c7d92d093b2e2b66395fe8a81823b786e6aa3a02
ty202500538 <ty202500538@wmsu.edu.ph>
2026-05-03 00:27:34 +0800

    Create P2_Q7_2.cpp

---
+++ P2_Q7_2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+bool isPerfect(int num) {
+    if (num <= 0) return false;
+    int sum = 0;
+    for (int i = 1; i < num; i++) {
+        if (num % i == 0) {
+          sum += i;
+        }
+    }
+    return sum == num;
+   }
+
+int main() {
+    ifstream inFile("input.txt");
+    ofstream outFile("output.txt");
+    int num;
+
+    while (inFile >> num) {
+        if (isPerfect(num)) {
+          outFile << num << endl;
+        }
+       }
+
+    inFile.close();
+    outFile.close();
+    return 0;
+}
